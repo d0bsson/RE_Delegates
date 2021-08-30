3b6e7cbfff8899ce60ce800b897cd37e2b6238ac
David <ber.dev@yandex.ru>
2021-08-30 19:22:12 +0300

    all done

--- RE_RGBsliders/ViewController.swift
+++ RE_RGBsliders/ViewController.swift
@@ -8,12 +8,39 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var mainView: UIView!
+    
+    @IBOutlet weak var redSliderValue: UILabel!
+    @IBOutlet weak var greenSliderValue: UILabel!
+    @IBOutlet weak var blueSliderValue: UILabel!
+        
+    @IBOutlet weak var redSlider: UISlider!
+    @IBOutlet weak var greenSlider: UISlider!
+    @IBOutlet weak var blueSlider: UISlider!
+    
+    let cg: CGFloat = 0.44
+    let cg1: CGFloat = 0.33
+    let cg2: CGFloat = 0.22
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        redSliderChanched()
     }
 
-
+    @IBAction func redSliderChanched() {
+        
+        mainView.backgroundColor = UIColor(
+            red: CGFloat(redSlider.value),
+            green: CGFloat(greenSlider.value),
+            blue: CGFloat(blueSlider.value),
+            alpha: 1
+        )
+                
+        redSliderValue.text = String(format: "%.2f", redSlider.value)
+        greenSliderValue.text = String(format: "%.2f", greenSlider.value)
+        greenSliderValue.text = String(format: "%.2f", blueSlider.value)
+    }
+    
 }
 
